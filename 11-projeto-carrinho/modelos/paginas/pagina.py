cfba08e9e3e84661675afc6996cf4bc2b127f447
ubiratanfilho <ubfilho16@gmail.com>
2022-09-08 12:16:54 -0300

    Criação da classe Página

---
+++ 11-projeto-carrinho/modelos/paginas/pagina.py
@@ -0,0 +1,21 @@
+from tkinter import PhotoImage
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+
+class Pagina():
+    def _construir_base(self):
+        janela = ttk.Window(
+            title="Página",
+            size=(1440, 1024),
+        )
+        return janela
+    
+    def __init__(self) -> None:
+        self.base = self._construir_base()
+        
+    def run(self):
+        self.base.mainloop()
+        
+if __name__ == "__main__":
+    app = Pagina()
+    app.run()
