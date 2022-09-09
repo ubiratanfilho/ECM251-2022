d5d5f4977427085a664c415782d5cc2f17d1ac53
ubiratanfilho <ubfilho16@gmail.com>
2022-09-09 12:28:43 -0300

    Botão Carrinho

--- 11-projeto-carrinho/modelos/paginas/pagina_inicial.py
+++ 11-projeto-carrinho/modelos/paginas/pagina_inicial.py
@@ -9,13 +9,16 @@ class PaginaInicial(Pagina):
         
         
     def _criar_layout(self):
+        # barra superior
         barra_superior = ttk.Frame(self, width=1366, height=150, bootstyle="dark")
         barra_superior.pack(fill=X)
-        busca = ttk.Entry(barra_superior, width=200)
-        busca.pack(side=RIGHT, padx=20, pady=10)
         logo = ttk.Label(barra_superior, text="Ubira Tec", font=("Arial"),
                          bootstyle="inverse-dark")
         logo.pack(side=LEFT, padx=20, pady=10)
+        carrinho_botao = ttk.Button(barra_superior, text="Carrinho", width=10, bootstyle="secondary")
+        carrinho_botao.pack(side=RIGHT, padx=30, pady=10)
+        busca = ttk.Entry(barra_superior, width=150)
+        busca.pack(side=RIGHT, padx=10, pady=10)
         
         produtos_destaque = ttk.Frame(self, width=1366, height=300, bootstyle="secondary")
         produtos_destaque.pack()
