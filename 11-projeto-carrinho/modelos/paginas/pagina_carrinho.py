b61a50a8715eb0a48398d98ca3a3cd3dc2a247c3
ubiratanfilho <ubfilho16@gmail.com>
2022-09-09 15:52:33 -0300

    Adicionando frames em PaginaCarrinho

--- 11-projeto-carrinho/modelos/paginas/pagina_carrinho.py
+++ 11-projeto-carrinho/modelos/paginas/pagina_carrinho.py
@@ -9,9 +9,11 @@ class PaginaCarrinho(PaginaInicial):
     
     def _criar_layout(self):
         self._criar_barra_superior()
+        self._criar_carrinho()
         
     def _criar_carrinho(self):
-        pass
+        bloco = ttk.Frame(self, bootstyle="secondary", width=1000, height=1000)
+        bloco.pack(pady=20, fill=Y)
 
 if __name__ == "__main__":
     janela = Janela(title="Carrinho")
