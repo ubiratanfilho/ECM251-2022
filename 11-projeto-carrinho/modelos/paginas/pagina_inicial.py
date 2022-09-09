7e4a8f90e18c1ceffc4196631fa0a4cf47db8028
ubiratanfilho <ubfilho16@gmail.com>
2022-09-09 11:07:18 -0300

    Logo Página Inicial

--- 11-projeto-carrinho/modelos/paginas/pagina_inicial.py
+++ 11-projeto-carrinho/modelos/paginas/pagina_inicial.py
@@ -7,11 +7,15 @@ class PaginaInicial(Pagina):
         super().__init__(master, **kwargs)
         self._criar_layout()
         
+        
     def _criar_layout(self):
         barra_superior = ttk.Frame(self, width=1366, height=150, bootstyle="dark")
         barra_superior.pack(fill=X)
         busca = ttk.Entry(barra_superior, width=200)
         busca.pack(side=RIGHT, padx=20, pady=10)
+        logo = ttk.Label(barra_superior, text="Ubira Tec", font=("Arial"),
+                         bootstyle="inverse-dark")
+        logo.pack(side=LEFT, padx=20, pady=10)
         
         produtos_destaque = ttk.Frame(self, width=1366, height=300, bootstyle="secondary")
         produtos_destaque.pack()
