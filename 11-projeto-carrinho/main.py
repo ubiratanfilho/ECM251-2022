6bd034307a211f125721abd67c8ce6730bb8c57e
ubiratanfilho <ubfilho16@gmail.com>
2022-09-15 11:49:00 -0300

    Deletando virtual env

--- 11-projeto-carrinho/main.py
+++ 11-projeto-carrinho/main.py
@@ -1,14 +1,2 @@
-from modelos.item import Item
-from modelos.carrinho import Carrinho
 
-item1 = Item(
-    preco=10,
-    nome="Item 1",
-    descricao="Descrição do item 1"
-)
-item2 = Item(
-    preco=20,
-    nome="Item 2",
-    descricao="Descrição do item 2"
-)
 
