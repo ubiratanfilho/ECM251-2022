4dd6ff872a535af4032cf2e64050ec4022a3c040
ubiratanfilho <ubfilho16@gmail.com>
2022-09-24 12:45:45 -0300

    Autorizando apenas usuários logados visualizarem os produtos na página inicial

--- Projetos/Atividade3/src/Home.py
+++ Projetos/Atividade3/src/Home.py
@@ -1,3 +1,6 @@
+# Ubiratan da Motta Filho
+# R.A - 20.00928-3
+
 import streamlit as st
 from modules.item import Item
 from modules.usuarios.usuario import Usuario
@@ -21,6 +24,7 @@ if "estoque" not in st.session_state:
     p3 = Item(449.99, "Nike PG4 Gatorade", "Tênis de Basquete do Paul George","./imagens/pg4.png")
     st.session_state.estoque = [p1, p2, p3]
 
+### Página Inicial
 # Exibe o título da página inicial
 col1, col2 = st.columns(2)
 with col1:
@@ -28,20 +32,23 @@ with col1:
     st.markdown("### A única loja do seu ❤️💻")
 with col2:
     st.image("./imagens/Utec.png", width=200)
-    
-st.markdown("## Produtos") 
 
-# Exibe os produtos
-def exibe_produto(produto):
-    from PIL import Image
-    img = Image.open(produto.get_imagem())
-    st.image(img, width=200)
-    st.markdown(f"### {produto.get_nome()}")
-    st.markdown(f"{produto.get_descricao()}")
-    st.markdown(f"#### R${produto.get_preco()}")
-    button = st.button("Comprar 🛒", key=produto.get_nome())
-    if button:
-        st.session_state.carrinho.append(produto)
+if st.session_state.usuario != None:
+    st.markdown("## Produtos") 
 
-for produto in st.session_state.estoque:
-    exibe_produto(produto)
+    # Exibe os produtos
+    def exibe_produto(produto):
+        from PIL import Image
+        img = Image.open(produto.get_imagem())
+        st.image(img, width=200)
+        st.markdown(f"### {produto.get_nome()}")
+        st.markdown(f"{produto.get_descricao()}")
+        st.markdown(f"#### R${produto.get_preco()}")
+        button = st.button("Comprar 🛒", key=produto.get_nome())
+        if button:
+            st.session_state.carrinho.append(produto)
+
+    for produto in st.session_state.estoque:
+        exibe_produto(produto)
+else:
+    st.error("Você precisa estar logado para ver os produtos, vá até a tela de login e coloque seu usuário e senha")
