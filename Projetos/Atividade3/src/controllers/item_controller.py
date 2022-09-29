a81574c7706e6c516261e3adf58e5c8d667c10b4
ubiratanfilho <ubfilho16@gmail.com>
2022-09-29 08:35:30 -0300

    Padronizando sintaxe dos métodos

--- Projetos/Atividade3/src/controllers/item_controller.py
+++ Projetos/Atividade3/src/controllers/item_controller.py
@@ -1,2 +1,9 @@
 # Ubiratan da Motta Filho
 # R.A - 20.00928-3
+
+import streamlit as st
+from models.item import Item
+
+class ItemController:
+    def __init__(self):
+        pass
--- Projetos/Atividade3/src/models/usuario.py
+++ Projetos/Atividade3/src/models/usuario.py
@@ -1,11 +1,11 @@
 class Usuario():
-    def __init__(self, user_name, password, email) -> None:
+    def __init__(self, user_name, password, email):
         self._user_name = user_name
         self._password = password
         self._email = email
         
-    def get_user_name(self) -> str:
+    def get_user_name(self):
         return self._user_name
     
-    def check_password(self, password) -> bool:
+    def check_password(self, password):
         return self._password == password
