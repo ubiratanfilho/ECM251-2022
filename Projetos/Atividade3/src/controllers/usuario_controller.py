85db242f8258129e524ba9fe2c93e626052b714a
ubiratanfilho <ubfilho16@gmail.com>
2022-10-28 10:54:12 -0300

    Criação de UsuarioDAO

--- Projetos/Atividade3/src/controllers/usuario_controller.py
+++ Projetos/Atividade3/src/controllers/usuario_controller.py
@@ -5,25 +5,4 @@ import streamlit as st
 from models.usuario import Usuario
 
 class UsuarioController:
-    def __init__(self):
-        if "usuario" not in st.session_state:
-            st.session_state.usuario = None
-        if "all_users" not in st.session_state:
-            # Usuários no banco de dados
-            u1 = Usuario("bira", "123", "bira@gmail.com")
-            u2 = Usuario("jordan", "123", "jordan@gmail.com")
-            u3 = Usuario("lebron", "123", "lebron@gmail.com")
-            st.session_state.all_users = [u1, u2, u3]
-            
-    def check_login(self, usuario, senha):
-        usuario_correto = None
-        for u in st.session_state.all_users:
-            if usuario == u.get_user_name() and u.check_password(senha):
-                usuario_correto = u
-                break
-        if usuario_correto == None:
-            st.error("Usuário ou senha incorretos ❌")
-            st.session_state.usuario = None
-        else:
-            st.success("Login realizado com sucesso ✔️")
-            st.session_state.usuario = usuario_correto
+    pass
---
+++ Projetos/Atividade3/src/dao/usuario_dao.py
@@ -0,0 +1,4 @@
+from models.usuario import Usuario
+
+class UsuarioDAO:
+    pass
