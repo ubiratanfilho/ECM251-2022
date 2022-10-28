8ca3d49cd022280b8e31daf2747bf308c5482ede
ubiratanfilho <ubfilho16@gmail.com>
2022-10-28 10:52:22 -0300

    Getters e Setter Usuario

--- Projetos/Atividade3/src/models/usuario.py
+++ Projetos/Atividade3/src/models/usuario.py
@@ -4,8 +4,15 @@ class Usuario():
         self._password = password
         self._email = email
         
+    # Getters e Setters
     def get_user_name(self):
         return self._user_name
+    def get_email(self):
+        return self._email
+    def set_email(self, email):
+        self._email = email
+    def set_password(self, password):
+        self._password = password
     
     def check_password(self, password):
         return self._password == password
