a7c4cdc651e6ba524f0a94923026262c28ab4171
ubiratanfilho <ubfilho16@gmail.com>
2022-11-01 10:29:01 -0300

    Erro de usuario

--- Projetos/Atividade3/src/Home.py
+++ Projetos/Atividade3/src/Home.py
@@ -7,6 +7,7 @@ from models.usuario import Usuario
 from controllers.usuario_controller import UsuarioController
 from controllers.item_controller import ItemController
 
+# Inicializando controllers e variáveis globais
 if 'usuario_controller' not in st.session_state:
     st.session_state.usuario_controller = UsuarioController()
 if 'item_controller' not in st.session_state:
@@ -25,3 +26,5 @@ with col2:
     st.image("https://dsm01pap009files.storage.live.com/y4maINQ8Xgz7FG0AUxeLQh4CpPFdB2g4_GYFvYG9neIhjBpQCQ6a9ldy86RSL14BqQ_rlCLTMm2X5ubVYXiWFirbJqxofB_9avqa2yE8BZpovEPpPCcSysf3_IDL5wgacBwDHCi2JcbGGJDzmqQblo5xASBEY-d2xZrKy7NBBt1qHgPEh0TeU95X6M1ZUFYkGnR?width=1000&height=1000&cropmode=none", width=200)
 
 # Checa se usuário está logado
+if st.session_state.usuario == None:
+    st.error("Você precisa estar logado para acessar essa página!")
