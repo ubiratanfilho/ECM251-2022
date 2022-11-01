931e9c47fea4e3f7148295cca460452845ecebcc
ubiratanfilho <ubfilho16@gmail.com>
2022-11-01 10:56:05 -0300

    Limpando database

--- Projetos/Atividade3/src/test.py
+++ Projetos/Atividade3/src/test.py
@@ -1,38 +1,8 @@
 # from dao.item_dao import ItemDAO
 # from models.item import Item
 # from controllers.item_controller import ItemController
-from dao.usuario_dao import UsuarioDAO
-from models.usuario import Usuario
+from controllers.item_controller import ItemController
 from controllers.usuario_controller import UsuarioController
 
-# Criação de itens
-# p1 = Item("UbiraTec Washer", "Lavadora de tênis e sapatos", 354.99, "./imagens/washer.png")
-# p2 = Item("Samsung Neo G9", "Monitor Gamer Samsung Curvado", 9999.99, "./imagens/neog9.jfif")
-# p3 = Item("Nike PG4 Gatorade", "Tênis de Basquete do Paul George", 449.99, "./imagens/pg4.png")
-
-# Criação de usuários
-u1 = Usuario("ubiratan", "ubiratan@gmail.com", "123456")
-u2 = Usuario("joao", "joao@gmail.com", "123456")
-u3 = Usuario("maria", "maria@gmail.com", "123456")
-
-# Inserção de usuários
-controller = UsuarioController()
-controller.limpar_tabela()
-controller.inserir_usuario(u1) 
-controller.inserir_usuario(u2)
-controller.inserir_usuario(u3)
-
-# Exibição de usuários
-for usuario in controller.get_all():
-    print(usuario)
-print()
-    
-# Delete de usuário
-controller.deletar_usuario(u1)
-
-# Exibição de usuários
-for usuario in controller.get_all():
-    print(usuario)
-    
-# Limpando tabela
-controller.limpar_tabela()
+UsuarioController().limpar_tabela()
+ItemController().limpar_tabela()
