b9f6e38dcf4ef32acb155c94eaf580d84834fd93
ubiratanfilho <ubfilho16@gmail.com>
2022-11-06 21:19:40 -0300

    Deletando test.py

--- Projetos/Atividade3/src/test.py
+++
@@ -1,9 +0,0 @@
-# from dao.item_dao import ItemDAO
-# from models.item import Item
-# from controllers.item_controller import ItemController
-from controllers.item_controller import ItemController
-from controllers.usuario_controller import UsuarioController
-from models.usuario import Usuario
-from models.item import Item
-
-ItemController().limpar_tabela()
